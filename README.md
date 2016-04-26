9d479152084bf86ee8ab54ca70011405da23913e
Trygve <tnordberg@smm.org>
2016-04-26 14:55:19 -0500

    Boilerplate with initial structure.

--- README.md
+++
@@ -1,2 +0,0 @@
-# sound-fade-slider
-UI template for fading between sounds and images.
---
+++ js/main.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+
+  var xFadeSounds = [];
+
+  $('.cross-fade-group img').each(function() {
+
+    var audioPath = $(this).attr('data-audio');
+    var xPosition = $(this).attr('data-xPos');
+
+    $(this).css('left', parseInt(xPosition));
+
+    addXFadeSound('../' + audioPath, xPosition);
+
+  });
+
+  function addXFadeSound(url, x) {
+
+    var newSnd = new Howl({
+      src: [url],
+      autoplay: true,
+      loop: true,
+      volume: 0.0,
+    });
+
+    newSnd.xPos = x;
+
+    xFadeSounds.push(newSnd);
+
+  }
+
+  $('#slider').slider({
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  },
+});
+
+
+
+
+  // $(window).mousemove(function(e) {
+
+  //   var normX = e.pageX / $(window).width();
+
+  //   $('footer p').text('x:' + e.pageX + ' normX:' + normX);
+
+  //   var sortedArray = distanceSort(xFadeSounds, e.pageX);
+
+  //   for (var i = 1; i < sortedArray.length; i++) {
+
+  //     if (i >= 2) {
+  //       sortedArray[i].volume(0.0);
+  //     } else {
+  //       // Mix two closest sounds.
+  //       var maxdist = Math.abs(sortedArray[1].xPos - sortedArray[0].xPos);
+  //       var curdist = Math.abs(e.pageX - sortedArray[0].xPos);
+
+  //       var crossPoint = map(curdist, 0, maxdist, 0, 1);
+  //       console.log('cp' + crossPoint);
+  //       sortedArray[0].volume(1.0 - crossPoint);
+  //       sortedArray[1].volume(1.0 + crossPoint);
+  //     }
+
+  //   }
+
+  // });
+
+});
+
+// Utility functions
+function map(val, inMin, inMax, outMin, outMax) {
+  return (val - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+function distanceSort(arr, target) {
+
+  return arr.sort(function(a, b) {
+
+    var distance1 = Math.abs(target - a.xPos);
+    var distance2 = Math.abs(target - b.xPos);
+
+    return distance1 == distance2 ? 0 : (distance1 > distance2 ? 1 : -1);
+
+  });
+
+}
